032a4a67ce6b58326769e9bdc5418a70a92b82b7
Santiago Palladino <spalladino@gmail.com>
2016-08-02 13:15:56 -0300

    Redirect to path, not URL

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -56,7 +56,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to items_path, notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
